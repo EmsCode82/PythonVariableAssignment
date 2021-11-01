cac4c90c430ea28ddb380065f7cd45bb1bd0b39d
EmsCode82 <emsgithub@gmail.com>
2021-11-01 12:34:15 -0500

    .

---
+++ variableAssignment.py
@@ -0,0 +1,29 @@
+mydogs = 2
+mydogs = ["Sammy", "Frank"]
+print (mydogs)
+
+a = 5
+a = 10
+
+print (a)
+
+a = a + a
+print (a)
+
+print  (type (a))
+
+a = 30.7
+
+print (type(a))
+print (a)
+
+my_income = 1000000
+my_taxrate = 0.20
+my_taxes = my_income * my_taxrate
+
+print (my_taxes)
+
+
+
+
+
